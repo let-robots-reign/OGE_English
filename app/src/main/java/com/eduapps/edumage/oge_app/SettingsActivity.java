a9daac2f07263b72481f57bde4c27df9bae6c0bc
let-robots-reign <zotov.alexei.1@yandex.ru>
2019-01-25 12:03:55 +0400

    Feature: information screen Fix: lock orientation

--- app/src/main/java/com/eduapps/edumage/oge_app/SettingsActivity.java
+++ app/src/main/java/com/eduapps/edumage/oge_app/SettingsActivity.java
@@ -1,14 +1,31 @@
 package com.eduapps.edumage.oge_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(R.layout.app_information);
+
+        ImageView emailButton = findViewById(R.id.email_icon);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL  , new String[] {"mobile.4.education@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "ОГЭ Английский");
+
+                startActivity(Intent.createChooser(intent, "Email via..."));
+            }
+        });
     }
 }
