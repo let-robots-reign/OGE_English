b17fe4e3bfffd7fb5c0f73b4c01b0cca1987e379
let-robots-reign <zotov.alexei.1@yandex.ru>
2019-02-01 21:08:19 +0400

    trying to add admob to project

--- app/src/main/java/com/eduapps/edumage/oge_app/ReadingActivity.java
+++ app/src/main/java/com/eduapps/edumage/oge_app/ReadingActivity.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +21,10 @@ public class ReadingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category_list);
+
+        AdView adView = findViewById(R.id.adView_topics);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
         RecyclerView readingTopicsList = findViewById(R.id.topics_list);
 
